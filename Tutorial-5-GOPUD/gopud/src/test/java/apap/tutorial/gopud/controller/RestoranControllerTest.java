f73534599b81d400af819b271d4256234f922f01
Farhan Azyumardhi Azmi <sonofkyrat99@gmail.com>
2019-12-29 04:37:55 +0700

    [Tutorial-5-GOPUD] Added more tests on RestoranController class

--- Tutorial-5-GOPUD/gopud/src/test/java/apap/tutorial/gopud/controller/RestoranControllerTest.java
+++ Tutorial-5-GOPUD/gopud/src/test/java/apap/tutorial/gopud/controller/RestoranControllerTest.java
@@ -1,11 +1,13 @@
 package apap.tutorial.gopud.controller;
 
 import apap.tutorial.gopud.model.RestoranModel;
+import apap.tutorial.gopud.repository.RestoranDb;
 import apap.tutorial.gopud.service.MenuService;
 import apap.tutorial.gopud.service.RestoranService;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -17,6 +19,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.hamcrest.Matchers.*;
 import static org.mockito.Mockito.*;
@@ -30,6 +33,9 @@ public class RestoranControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @MockBean
+    private RestoranDb restoranDb;
 
     @MockBean
     @Qualifier("restoranServiceImpl")
@@ -104,5 +110,91 @@ public class RestoranControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(MockMvcResultMatchers.view().name("add-restoran"))
                 .andExpect(model().attribute("namaResto", is(nama)));
+    }
+
+    @Test
+    public void whenAddRestoranFormIsAccessedItShouldReturnTheCorrectView() throws Exception {
+        mockMvc.perform(get("/restoran/add"))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("form-add-restoran"))
+                .andExpect(content().string(Matchers.containsString("Tambah Restoran")))
+                .andExpect(model().attributeExists("restoran"));
+    }
+
+    @Test
+    public void whenASpecificRestoranViewIsAccessedItShouldShowTheRestoranData() throws Exception {
+        RestoranModel restoran = new RestoranModel();
+        restoran.setIdRestoran((long) 1);
+        restoran.setNama("Dummy nama");
+        restoran.setAlamat("Dummy alamat");
+        restoran.setNomorTelepon(420);
+        when(restoranService.getRestoranByIdRestoran((long) 1)).thenReturn(Optional.of(restoran));
+
+        mockMvc.perform(get("/restoran/view/?idRestoran=" + restoran.getIdRestoran()))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("view-restoran"))
+                .andExpect(model().attributeExists("resto"))
+                .andExpect(model().attributeExists("menuList"))
+                .andExpect(model().attribute("resto",
+                        hasProperty("idRestoran", is((long) 1))))
+                .andExpect(model().attribute("resto",
+                        hasProperty("nama", is("Dummy nama"))))
+                .andExpect(model().attribute("resto",
+                        hasProperty("alamat", is("Dummy alamat"))))
+                .andExpect(model().attribute("resto",
+                        hasProperty("nomorTelepon", is(420))));
+    }
+
+    @Test
+    public void whenChangeExistingRestoranPageIsAccessedItShouldReturnTheCorrectViewWithPopulatedFormField()
+            throws Exception {
+        RestoranModel restoran = new RestoranModel();
+        restoran.setIdRestoran((long) 1);
+        restoran.setNama("Dummy nama");
+        restoran.setAlamat("Dummy alamat");
+        restoran.setNomorTelepon(420);
+        when(restoranService.getRestoranByIdRestoran((long) 1)).thenReturn(Optional.of(restoran));
+
+        mockMvc.perform(get("/restoran/change/" + restoran.getIdRestoran()))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("form-change-restoran"))
+                .andExpect(model().attributeExists("restoran"))
+                .andExpect(content().string(Matchers.containsString("Change Data Restoran")))
+                .andExpect(model().attribute("restoran",
+                        hasProperty("idRestoran", is((long) 1))))
+                .andExpect(model().attribute("restoran",
+                        hasProperty("nama", is("Dummy nama"))))
+                .andExpect(model().attribute("restoran",
+                        hasProperty("alamat", is("Dummy alamat"))))
+                .andExpect(model().attribute("restoran",
+                        hasProperty("nomorTelepon", is(420))));
+    }
+
+    @Test
+    public void whenChangeNonExistingRestoranPageIsAccessedItShouldReturnTheCorrectViewWithoutForm()
+            throws Exception {
+        when(restoranService.getRestoranByIdRestoran((long) 1)).thenReturn(Optional.empty());
+
+        mockMvc.perform(get("/restoran/change/1"))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("form-change-restoran"))
+                .andExpect(model().attributeDoesNotExist("restoran"))
+                .andExpect(content().string(Matchers.containsString("Restoran tidak ditemukan")));
+    }
+
+    @Test
+    public void whenDeleteRestoranByIdItShouldReturnTheRightView() throws Exception {
+        RestoranModel restoran = new RestoranModel();
+        restoran.setIdRestoran((long) 1);
+        restoran.setNama("Dummy restoran");
+        restoran.setAlamat("Dummy alamat");
+        restoran.setNomorTelepon(100);
+        when(restoranService.getRestoranByIdRestoran((restoran.getIdRestoran()))).thenReturn(Optional.of(restoran));
+
+        mockMvc.perform(post("/restoran/delete/" + restoran.getIdRestoran()))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("delete-restoran"))
+                .andExpect(model().attributeExists("restoran"));
+        verify(restoranService, times(1)).deleteByIdRestoran(restoran.getIdRestoran());
     }
 }
