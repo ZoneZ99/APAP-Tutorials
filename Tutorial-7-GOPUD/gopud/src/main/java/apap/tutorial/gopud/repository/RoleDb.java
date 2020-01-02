bab63c6d2ca978ee003041967e0ae347f4db8209
Farhan Azyumardhi Azmi <sonofkyrat99@gmail.com>
2020-01-02 13:22:10 +0700

    [Tutorial-7-GOPUD] Implemented add user feature

---
+++ Tutorial-7-GOPUD/gopud/src/main/java/apap/tutorial/gopud/repository/RoleDb.java
@@ -0,0 +1,8 @@
+package apap.tutorial.gopud.repository;
+
+import apap.tutorial.gopud.model.RoleModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleDb extends JpaRepository<RoleModel, Long> {
+
+}
---
+++ Tutorial-7-GOPUD/gopud/src/main/java/apap/tutorial/gopud/service/UserServiceImpl.java
@@ -0,0 +1,27 @@
+package apap.tutorial.gopud.service;
+
+import apap.tutorial.gopud.model.UserModel;
+import apap.tutorial.gopud.repository.UserDb;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserDb userDb;
+
+    @Override
+    public UserModel addUser(UserModel user) {
+        String rawPassword = encrypt(user.getPassword());
+        user.setPassword(rawPassword);
+        return userDb.save(user);
+    }
+
+    @Override
+    public String encrypt(String rawPassword) {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        return passwordEncoder.encode(rawPassword);
+    }
+}
