4c5162680809021d0d1736114485d76b9ee5635f
Farhan Azyumardhi Azmi <sonofkyrat99@gmail.com>
2020-01-04 18:11:23 +0700

    [Tutorial-9-GOPUD] Changed gopud project to Tutorial-6's

--- Tutorial-9-GOPUD/gopud/src/main/java/apap/tutorial/gopud/repository/RoleDb.java
+++
@@ -1,8 +0,0 @@
-package apap.tutorial.gopud.repository;
-
-import apap.tutorial.gopud.model.RoleModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleDb extends JpaRepository<RoleModel, Long> {
-
-}
