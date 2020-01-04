382f39d0027018dd7777990b45a689553885cb3d
Farhan Azyumardhi Azmi <sonofkyrat99@gmail.com>
2020-01-04 12:47:40 +0700

    [Tutorial-8] Implemented stateful menu list

--- Tutorial-8/frontend/src/App.js
+++ Tutorial-8/frontend/src/App.js
@@ -4,7 +4,27 @@ import List from "./components/List";
 import dummyItems from "./items.json";
 
 export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            favItems: []
+        };
+    }
+
+    handleItemClick = item => {
+        const newItems = [...this.state.favItems];
+        const newItem = {...item};
+        const targetInd = newItems.findIndex(it => it.id === newItem.id);
+        if (targetInd < 0) {
+            newItems.push(newItem);
+        } else {
+            newItems.splice(targetInd, 1);
+        }
+        this.setState({favItems: newItems});
+    };
+
     render() {
+        const {favItems} = this.state;
         return (
             <div className="container-fluid">
                 <h1 className="text-center">
@@ -17,8 +37,14 @@ export default class App extends React.Component {
                             <List
                                 title="Our Menu"
                                 items={dummyItems}
-                                onItemClick={() => {
-                                }}
+                                onItemClick={this.handleItemClick}
+                            />
+                        </div>
+                        <div className="col-sm">
+                            <List
+                                title="My Favorite"
+                                items={favItems}
+                                onItemClick={this.handleItemClick}
                             />
                         </div>
                     </div>
