71f5ae2d780eb754156791c82e10528ac97014fc
Farhan Azyumardhi Azmi <sonofkyrat99@gmail.com>
2020-01-04 12:58:34 +0700

    [Tutorial-8] Implemented functional version of stateful menu list

--- Tutorial-8/frontend/src/index.js
+++ Tutorial-8/frontend/src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import AppClass from './App';
+import AppFunc from "./AppFunc";
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <React.Fragment>
+        <div className="mt-5" />
+        <AppClass />
+        <div className="mt-5 mb-5" />
+        <AppFunc />
+        <div className="mt-5 mb-5" />
+    </React.Fragment>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
